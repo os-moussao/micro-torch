4d5805c76b240849673021e019f68ed970e1f67c
Oussama Moussaoui <omoussaoui1337@gmail.com>
2026-04-02 00:09:56 +0100

    chore: make randome split dynamic

--- microtorch/preprocessing/random_split.py
+++ microtorch/preprocessing/random_split.py
@@ -3,27 +3,26 @@ import numpy as np
 import math
 
 
-def random_split(X: np.ndarray, y: np.ndarray, ratios: list[float], seed: Optional[int] = None):
+def random_split(*arrays: np.ndarray, ratios: list[float], seed: Optional[int] = None):
+    assert len(arrays) > 0, "At least one array must be provided"
     assert len(ratios) >= 2, "Ratios must have at least 2 elements"
     assert sum(ratios) == 1, "Sum of ratios must be equal to 1"
-    assert len(X) == len(y), "X and y must have the same length"
+    
+    n = len(arrays[0])
+    for arr in arrays:
+        assert len(arr) == n, "All arrays must have the same length"
 
     gen = np.random.default_rng(seed) if seed is not None else np.random
 
-    n = len(X)
     shuffle = gen.permutation(n)
-    X = X[shuffle]
-    y = y[shuffle]
+    shuffled_arrays = [arr[shuffle] for arr in arrays]
 
-    split: list[tuple[np.ndarray, np.ndarray]] = []
+    split: list[list[np.ndarray]] = []
     start = 0
     for i in range(len(ratios)):
         sz = math.ceil(ratios[i] * n)
         end = start + sz if i != len(ratios) - 1 else n
-        split.append((
-            X[start:end],
-            y[start:end]
-        ))
+        split.append([arr[start:end] for arr in shuffled_arrays])
         start = end
 
     return split
