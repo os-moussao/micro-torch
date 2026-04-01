a6feca6bc4eb5386ef99a2ae5305bbf9f664ad77
Oussama Moussaoui <omoussaoui1337@gmail.com>
2026-04-01 23:06:35 +0100

    chore: organize dependencies

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 python3 -m venv .venv
 source .venv/bin/activate
-pip install requirements.txt
+pip install -r ./requirements/microtorch-requirements.txt
+pip install -r ./requirements/testing-requirements.txt
+pip install . # to add microtorch as a package for testing
