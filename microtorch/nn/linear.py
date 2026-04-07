bd7e93d5cc64c54cf25d9611a89ed5ba76170a92
Oussama Moussaoui <omoussaoui1337@gmail.com>
2026-04-07 14:09:57 +0100

    chore: add Xavier & He initialization

--- microtorch/nn/linear.py
+++ microtorch/nn/linear.py
@@ -1,15 +1,21 @@
-from ast import List
 from typing import Literal, Optional
 from microtorch.autograd.autograd import Grad
 from microtorch.functions.sigmoid import sigmoid
 from microtorch.nn.base_layer import Layer
+import numpy as np
 
 
 class Linear(Layer):
-    def __init__(self, in_features: int, out_features: int, bias=True, activation: Optional[Literal['relu', 'sigmoid']] = None):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bias=True,
+        activation: Optional[Literal['relu', 'sigmoid']] = None,
+    ):
         self.in_features = in_features
         self.out_features = out_features
-        self.weight = Grad.rand((in_features, out_features))
+        self.weight = self._initialize_weights(activation)
         self.bias = Grad.zeros((1, out_features)) if bias else None
         self.activation = activation
 
@@ -31,3 +37,17 @@ class Linear(Layer):
 
     def parameters(self):
         return self._parameters
+
+    def _initialize_weights(self, activation):
+        # use he initialization for relu activation
+        if activation == 'relu':
+            bound = np.sqrt(6.0 / self.in_features)
+
+        # use xavier initialization for sigmoid/tanh activations
+        else:
+            bound = np.sqrt(6.0 / (self.in_features + self.out_features))
+
+        raw_weights = np.random.uniform(-bound, bound,
+                                        size=(self.in_features, self.out_features))
+
+        return Grad.array(raw_weights)
