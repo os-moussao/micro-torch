fix: softmax

microtorch/functions/softmax.py
@@ -3,5 +3,6 @@ from microtorch.autograd.autograd import Grad
 
 
 def softmax(y: np.ndarray) -> np.ndarray:
-    y_exp = Grad.exp(y) if y.dtype == 'object' else np.exp(y)
-    return y_exp / y_exp.sum()
+    y_max = y.max(axis=1, keepdims=True)
+    y_exp = Grad.exp(y - y_max) if y.dtype == 'object' else np.exp(y - y_max)
+    return y_exp / y_exp.sum(axis=1, keepdims=True)