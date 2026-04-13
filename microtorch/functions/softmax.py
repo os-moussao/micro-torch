f25e9c2900ba8df5b1348a1a8f18c8896cd2a433
Oussama Moussaoui <omoussaoui1337@gmail.com>
2026-04-13 16:37:37 +0100

    refactor functions module

--- microtorch/functions/softmax.py
+++ microtorch/functions/softmax.py
@@ -1,8 +1,10 @@
 import numpy as np
 from microtorch.autograd.autograd import Grad
 
+def get_exp(y: np.ndarray) -> np.ndarray:
+    return Grad.exp(y) if y.dtype == 'object' else np.exp(y)
 
 def softmax(y: np.ndarray) -> np.ndarray:
     y_max = y.max(axis=1, keepdims=True)
-    y_exp = Grad.exp(y - y_max) if y.dtype == 'object' else np.exp(y - y_max)
-    return y_exp / y_exp.sum(axis=1, keepdims=True)
+    exp = get_exp(y - y_max)
+    return exp / exp.sum(axis=1, keepdims=True)
