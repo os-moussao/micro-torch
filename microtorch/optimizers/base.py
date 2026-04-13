6535075b1726954fc84c1407be93deec8c5e64d9
Oussama Moussaoui <omoussaoui1337@gmail.com>
2026-04-13 16:40:37 +0100

    refactor: make optimizer an iterable

--- microtorch/optimizers/base.py
+++ microtorch/optimizers/base.py
@@ -1,19 +1,31 @@
 from abc import ABC, abstractmethod
 from typing import List
 from microtorch.autograd.autograd import Grad
+from microtorch.nn.base_model import Model
 
 
 class Optimizer(ABC):
-    def __init__(self, params: List[Grad], lr: float = 0.01):
-        self.params = params
+    def __init__(self, model: Model, lr: float = 0.01):
         self.lr = lr
+        self.model = model
+        self.len = len(model.parameters())
 
     @abstractmethod
     def step(self):
         """Update parameters based on their gradients."""
         pass
 
+    def parameters(self):
+        return self.model.parameters()
+
     def zero_grad(self):
         """Zero out the gradients of all parameters."""
-        for param in self.params:
+        for param in self.parameters():
             param.grad = 0
+
+    def __iter__(self):
+        for param in self.parameters():
+            yield param
+
+    def __len__(self):
+        return self.len
--- microtorch/optimizers/sgd.py
+++ microtorch/optimizers/sgd.py
@@ -1,11 +1,12 @@
 from microtorch.autograd.autograd import Grad
+from microtorch.nn.base_model import Model
 from microtorch.optimizers.base import Optimizer
 
 
 class SGD(Optimizer):
-    def __init__(self, params: list[Grad], lr = 0.01):
-        super().__init__(params, lr)
+    def __init__(self, model: Model, lr = 0.01):
+        super().__init__(model, lr)
 
     def step(self):
-        for param in self.params:
+        for param in self.parameters():
             param.val -= self.lr * param.grad
